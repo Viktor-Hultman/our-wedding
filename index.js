5f8dc20de6ecc2f0bf6a8fe2eb78d49612d7ae12
Viktor Hultman <viktor-hultman@hotmail.com>
2021-11-20 09:57:42 +0100

    Fixed hero section with img and text, so now everything is reponsive.

---
+++ index.js
@@ -0,0 +1,6 @@
+const startLink =  document.querySelector(".startsida-nav-item")
+
+function toSite(string) {
+    let fullPage = string + ".html"
+    window.location.href = fullPage
+}
---
+++ önskelista.js
@@ -0,0 +1,4 @@
+function toSite(string) {
+    let fullPage = string + ".html"
+    window.location.href = fullPage
+}
