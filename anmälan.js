76319b3bdeab5108ad0a12f3f6913afc137fc9bc
Viktor Hultman <viktor-hultman@hotmail.com>
2021-12-13 17:39:37 +0100

    added all the form fields, one for each person (up to 5), added js code to show and hide the forms depending on how many persons have been selected

--- anmälan.js
+++ anmälan.js
@@ -2,3 +2,20 @@ function toSite(string) {
     let fullPage = string + ".html"
     window.location.href = fullPage
 }
+
+let antalSelect = document.querySelector("#antal")
+let personForms = document.getElementsByClassName("person")
+
+antalSelect.addEventListener("change", function (){
+    let antal = Number(antalSelect.value)
+    for(let i = 0; i < personForms.length; i++){
+        if(i < antal) {
+            console.log("Show person" + [i]);
+            personForms[i].classList.remove("hide")
+        } else {
+            console.log(antal);
+            console.log("Hide person" + [i]);
+            personForms[i].classList.add("hide")
+        }
+    }
+})
