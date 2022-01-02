dea8f6de81af1719fffcb14d4c25cc1341b4a7a8
Viktor Hultman <viktor-hultman@hotmail.com>
2022-01-02 22:48:43 +0100

    Added hamburger menu that fully works, changes some small sizes, paddings & margins

--- anmälan.js
+++ anmälan.js
@@ -3,6 +3,24 @@ function toSite(string) {
     window.location.href = fullPage
 }
 
+let menu = document.querySelector(".hamburger-menu")
+let burger = document.querySelector(".hamburger")
+let closeMenu = document.querySelector(".menu-exit")
+
+burger.addEventListener("click", function() {
+    if(menu.classList.contains("reset-animation")){
+        menu.classList.remove("reset-animation")
+    }
+    menu.classList.add("dropdown-animation")
+})
+
+closeMenu.addEventListener("click", function() {
+    if(menu.classList.contains("dropdown-animation")){
+        menu.classList.remove("dropdown-animation")
+    }
+    menu.classList.add("reset-animation")
+})
+
 let antalSelect = document.querySelector("#antal")
 let personForms = document.querySelectorAll(".person")
 
