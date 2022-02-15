bc597b38a0261dd77a72d4d7cbd371cfb1313a03
Viktor Hultman <viktor.hultman@student.kyh.se>
2022-02-15 19:09:47 +0100

    Update önskelista.js

--- önskelista.js
+++ önskelista.js
@@ -5,7 +5,6 @@ let önskelista = [
         "inköpsställe": "ELGIGANTEN",
         "länk": "https://www.elgiganten.se/product/hem-och-hushall/dammsugare-och-rengoring/skaftdammsugare/370862/dyson-omni-glide-dammsugare-37013301-lila",
         "harLänk": true,
-        "tillgänglig": true,
         "tillgänglig": true
     },
     {
